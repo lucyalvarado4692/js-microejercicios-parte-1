5ae63c699a70652629d3b114257c3885509f5d52
lucyalvarado4692 <104829949+lucyalvarado4692@users.noreply.github.com>
2022-09-10 14:39:05 +0200

    cambios index, readme y js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# js-microejercicios-parte-1
+# Microejercios JavaScript parte 1
+
+Aca estan unos ejercicios simples de JavaScript
+
+[Visualizar en el servidor GitHub](https://lucyalvarado4692.github.io/js-microejercicios-parte-1/)
+
+Created By: [Lucy Elena Alvarado Rodriguez](https://github.com/lucyalvarado4692)
--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-/*1.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World».*/
-document.write("Hello World");
-/*2.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.*/
-let number3 = 3;
-let number5 = 5;
-document.write(number3 + number5);
+/*1.- Escribe un programa que escriba en la pantalla un texto que diga «Hello World».*/
+let click1 = document.getElementById("click1");
+click1.onclick = click1Action;
+function click1Action() {
+  window.alert("Hello World");
+  let result = document.getElementById("result1");
+  result.innerHTML = "Hello World";
+  return result;
+}
+// document.write("Hello World");
+/*2.- Escribe un programa que escriba en la pantalla el resultado de sumar 3 + 5.*/
+let click2 = document.getElementById("click2");
+click2.onclick = click2Action;
+function click2Action() {
+  let number3 = 3;
+  let number5 = 5;
+  window.alert(number3 + number5);
+  let result = document.getElementById("result2");
+  result.innerHTML = number3 + number5;
+  return result;
+}
 /*3.- Escribe un programa que pida el nombre del usuario con un prompt o utilizando un input y escriba un texto que diga «Hola nombre Usuario»*/
-let myName = prompt("Escribe tu nombre");
-document.write("Hola " + myName);
+let click3 = document.getElementById("click3");
+click3.onclick = click3Action;
+function click3Action() {
+  let myName = prompt("Escribe tu nombre");
+  window.alert("Hola " + myName);
+  let result = document.getElementById("result3");
+  result.innerHTML = "Hola " + myName;
+  return result;
+}
 /*4.- Escribe un programa que pida un número, pida otro número y escriba el resultado de sumar estos dos números.*/
-let firstNumber = prompt("Escribe un número");
-let secondNumber = prompt("Escribe otro número");
-document.write(
-  "La suma es: " + (parseInt(firstNumber) + parseInt(secondNumber))
-);
+let click4 = document.getElementById("click4");
+click4.onclick = click4Action;
+function click4Action() {
+  let firstNumber = prompt("Escribe un número");
+  let secondNumber = prompt("Escribe otro número");
+  window.alert(
+    "La suma es: " + (parseInt(firstNumber) + parseInt(secondNumber))
+  );
+  let result = document.getElementById("result4");
+  result.innerHTML =
+    "La suma es: " + (parseInt(firstNumber) + parseInt(secondNumber));
+  return result;
+}
